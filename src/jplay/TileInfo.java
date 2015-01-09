abd608aefa71728ffb54a55a3ead114b348efb39
Victor Hugo Vieira de Sousa <vhv.sousa@gmail.com>
2015-01-09 08:48:48 -0300

    First Commit! This version is 2.1, developed by teachers and students of UFF. From www2.ic.uff.br/jplay

---
+++ src/jplay/TileInfo.java
@@ -0,0 +1,23 @@
+package jplay;
+/*
+ * The use this code commercially must only be done with permission of the author.
+ * Any modification shall be advised and sent to the author.
+ * The author is not responsible for any problem therefrom the use of this frameWork.
+ *
+ * @author Gefersom Cardoso Lima
+ * Federal Fluminense University
+ * Computer Science
+ */
+
+
+/**
+ * Class used to handle TileInfo.
+ */
+
+public class TileInfo extends GameObject
+{
+    /***
+     * Number used to identify the Tile.
+     */
+    public int id;
+}
